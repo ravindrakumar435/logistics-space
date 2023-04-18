55058466bf3327a139a567f5fd411435e9bfc53d
Ravindra <ravindra.y@imaginnovate.com>
2023-04-18 20:51:46 +0530

    add repo

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCGWo7CqEVrKFpf_dIfHq-Dn0F24fkXOXo",
+  authDomain: "technical-hub-e6f73.firebaseapp.com",
+  projectId: "technical-hub-e6f73",
+  storageBucket: "technical-hub-e6f73.appspot.com",
+  messagingSenderId: "1012983397517",
+  appId: "1:1012983397517:web:dc32704d7d4821c47af6c4",
+  measurementId: "G-P70XGHX9WZ"
+  }
+};
